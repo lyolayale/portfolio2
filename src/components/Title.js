aeca50fd269b792af2a3fcb96285bde445a45025
Eric McKee <lyolayale19@gmail.com>
2023-04-12 22:48:31 -0500

    Created a fade animated for Title component h3 -> title.

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,26 +4,46 @@ import React from "react";
 import TITLES from "../data/title";
 
 export default class Title extends React.Component {
-  state = { titleIndex: 0 };
+  state = { titleIndex: 0, fadeIn: true };
 
   componentDidMount() {
+    // initial fade-in -> false
+    this.timeout = setTimeout(() => {
+      this.setState(prev => ({ ...prev, fadeIn: false }));
+    }, 2000);
+
     this.animateTitles();
   }
 
   componentWillUnmount() {
+    // clear interval/timeout clean up
     clearInterval(this.titleInterval);
+    clearTimeout(this.timeout);
   }
 
   animateTitles = () => {
+    // Switching titles : using % to set back to orginal index of title -> 4 % 4 = 0
     this.titleInterval = setInterval(() => {
       const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
 
-      this.setState({ titleIndex });
+      // fade-in -> true
+      this.setState({ titleIndex, fadeIn: true });
+
+      // fade-in -> false : after 2 seconds
+      this.timeout = setTimeout(() => {
+        this.setState(prev => ({ ...prev, fadeIn: false }));
+      }, 2000);
     }, 4000);
   };
 
   render() {
-    const title = TITLES[this.state.titleIndex];
-    return <h3 className="title">I am {title}</h3>;
+    const { fadeIn, titleIndex } = this.state;
+
+    const title = TITLES[titleIndex];
+    return (
+      <h3 className={fadeIn ? "title-fade-in" : "title-fade-out"}>
+        I am {title}
+      </h3>
+    );
   }
 }
